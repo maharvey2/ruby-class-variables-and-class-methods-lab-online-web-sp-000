074eea9fed325bd85f429c5ed7a965dc87d1f454
Mark Harvey <maharvey2@crimson.ua.edu>
2019-05-25 22:12:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,7 +24,17 @@ def self.artists
 end
 
 def self.artist_count
+  artist_count = {}
+  @@artists.each do |artist|
+    if artist_count[artist]
+      artist_count[artist] +=1
+    else
+      artist_count = 1
+    end
 end
+artist_count
+end
+
 
 def self.genre
 end
