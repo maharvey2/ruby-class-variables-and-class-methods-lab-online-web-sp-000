accfe4c01c5a85d22a989a634762392b6651688a
Mark Harvey <maharvey2@crimson.ua.edu>
2019-05-25 22:04:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,26 @@
 class Song
+  attr_accessor :name, :artist, :genre
 
 
+def initialize
 
+end
 
+def self.count
 
+end
 
+def self.artists
 
+end
+
+def self.artist_count
+end
+
+def self.genre
+end
+
+def self.genre_count
+end
 
 end
