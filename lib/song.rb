2da87a6fccc30d5c99e57a9a9dcae68aa9f45669
Mark Harvey <maharvey2@crimson.ua.edu>
2019-05-25 22:15:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,9 +37,18 @@ end
 
 
 def self.genre
+  @@genres.unique
 end
 
 def self.genre_count
+  genre_count = {}
+  @@genres.each do |genre|
+    if genre_count[genre]
+      genre_count[genre] +=1
+    else
+      genre_count[genre] = 1
 end
-
+end
+          genre_count
+end
 end
