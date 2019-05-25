a28b8e27bfa0d2bc77a06ba28a7f363056caf47c
Mark Harvey <maharvey2@crimson.ua.edu>
2019-05-25 22:19:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,53 +2,51 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
-  @@artist = []
+  @@artists = []
   @@genres = []
 
-def initialize(name, artist, genre)
-  @name = name
-  @artist = artist
-  @genre = genre
-  @@count +=1
-  @@genres << genre
-  @@artists << artist
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count +=1
+    @@genres << genre
+    @@artists << artist
+  end
 
-end
+  def self.count
+    @@count
+  end
 
-def self.count
-  @@count
-end
+  def self.artists
+    @@artists.uniq
+  end
 
-def self.artists
-  @@artists.unique
-end
-
-def self.artist_count
-  artist_count = {}
-  @@artists.each do |artist|
-    if artist_count[artist]
-      artist_count[artist] +=1
-    else
-      artist_count = 1
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1
+      end
     end
-end
-artist_count
-end
+    artist_count
+  end
 
+  def self.genres
+    @@genres.uniq
+  end
 
-def self.genre
-  @@genres.unique
-end
-
-def self.genre_count
-  genre_count = {}
-  @@genres.each do |genre|
-    if genre_count[genre]
-      genre_count[genre] +=1
-    else
-      genre_count[genre] = 1
-end
-end
-          genre_count
-end
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
 end
